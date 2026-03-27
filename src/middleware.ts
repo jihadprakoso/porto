3d702c892a832373686f59c216ad6453e9d929a1
jihadprakoso <jihadprakoso@gmail.com>
2026-03-27 10:37:47 +0700

    Update middleware.ts

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,22 +1,12 @@
 import NextAuth from "next-auth";
 import { authConfig } from "./auth.config";
-import { createClient } from "@/utils/supabase/middleware";
-
 // We must provide the secret here for Edge compatibility if not picked up from env
 const { auth } = NextAuth({
   ...authConfig,
   secret: process.env.AUTH_SECRET,
 });
 
-export default auth(async (req) => {
-  try {
-    const supabaseResponse = createClient(req);
-    return supabaseResponse;
-  } catch (e) {
-    console.error("Middleware error:", e);
-    return; // Fallback to normal flow
-  }
-});
+export default auth;
 
 export const config = {
   matcher: [
